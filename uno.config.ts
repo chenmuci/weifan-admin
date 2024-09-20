361a7e016cea48b244615f5d658de4c64a27d291
xiezhilong <xiezhilong@bestshchina.com>
2024-09-20 14:29:44 +0800

    修改unocss配置

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,28 @@
-import { defineConfig } from 'unocss'
+import {
+    defineConfig,
+    presetUno,
+    presetAttributify,
+    presetTypography,
+    presetIcons,
+    transformerDirectives,
+    transformerVariantGroup
+} from 'unocss'
 
 export default defineConfig({
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetTypography(),
+        presetIcons({
+            scale: 1.2,
+            warn: true,
+        })
+    ],
+    shortcuts: [
+        ['flex-center', 'flex items-center justify-center'],
+        ['flex-between', 'flex items-center justify-between'],
+        ['flex-end', 'flex items-end justify-between']
+    ],
+    transformers: [transformerDirectives(), transformerVariantGroup()],
     rules: [[/^p-(\d+)$/, (match) => ({ padding: `${match[1]}px` })]]
 })
