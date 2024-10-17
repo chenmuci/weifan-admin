0f64be05adc5b8c24fa3f11d3cb68853660b11ea
xiezhilong <xiezhilong@bestshchina.com>
2024-10-17 11:24:19 +0800

    表格操作封装

---
+++ src/components/table-operation/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <a-space>
+        <a-tooltip title="刷新">
+            <a-button type="text" @click="props.refresh">
+                <template #icon><my-icon name="RedoOutlined" /></template>
+            </a-button>
+        </a-tooltip>
+        <a-tooltip title="列设置">
+            <a-button type="text">
+                <template #icon><my-icon name="SettingOutlined" /></template>
+            </a-button>
+        </a-tooltip>
+    </a-space>
+</template>
+
+<script setup lang="ts">
+    const emit = defineEmits(['update:modelValue']);
+    const props = defineProps({
+        modelValue: {
+            type: Array,
+            default: []
+        },
+        refresh: {
+            type: Function,
+            required: true
+        },
+        tableId: {
+            type: Number,
+            require: true
+        }
+    })
+</script>
